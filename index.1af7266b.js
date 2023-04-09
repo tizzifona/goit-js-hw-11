5fa6edc0af5d80d6b1764f983964cf2725fb72b4
tizzifona <100047250+tizzifona@users.noreply.github.com>
2023-04-09 01:02:17 +0000

    Deploying to gh-pages from @ tizzifona/goit-js-hw-11@8832e3ec658b635db82b0cd52aa49cacf79cf1bc 🚀

---
+++ index.1af7266b.js
@@ -0,0 +1,2 @@
+const i=document.querySelector(".btn");document.addEventListener("scroll",(()=>{window.scrollY>400?i.style.visibility="visible":i.style.visibility="hidden"})),i.addEventListener("click",(()=>{window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.1af7266b.js.map
---
+++ index.b4c54e48.js
@@ -0,0 +1,2 @@
+!function(){var i=document.querySelector(".btn");document.addEventListener("scroll",(function(){window.scrollY>400?i.style.visibility="visible":i.style.visibility="hidden"})),i.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.b4c54e48.js.map
